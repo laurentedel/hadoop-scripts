a8916d5bac683e9d60c617e87acef89be0f4724a
Laurent Edel <laurent.edel@gmail.com>
2016-01-13 11:06:19 +0100

    Create backup_configs.sh
    one-shot backup of all configurations with Ambari API

---
+++ backup_configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AMBARI_HOST=sandbox.hortonworks.com
+CLUSTER_NAME=Sandbox
+AMBARI_USER=admin
+AMBARI_PASSWORD=admin
+AMBARI_PORT=8080
+timeNow=`date +%Y%m%d_%H%M%S`
+RESULT_DIR=/root/migrationHDP/configs.sh/$timeNow
+mkdir -p $RESULT_DIR
+for CONFIG_TYPE in `curl -s -u $AMBARI_USER:$AMBARI_PASSWORD http://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/?fields=Clusters/desired_configs | grep '" : {' | grep -v Clusters | grep -v desired_configs | cut -d'"' -f2`; do
+  echo "backuping $CONFIG_TYPE"
+  /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT get $AMBARI_HOST $CLUSTER_NAME $CONFIG_TYPE | grep '^"' | grep -v '^"properties" : {' | sed "1i ##### $config_type #####" >> $RESULT_DIR/all.conf
+done

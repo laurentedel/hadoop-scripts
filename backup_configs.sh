75cb2ebf0c750cb5ee6d70c9fb9048619f09f7a0
Laurent Edel <laurent.edel@gmail.com>
2016-04-15 14:49:20 +0200

    Update backup_configs.sh

--- backup_configs.sh
+++ backup_configs.sh
@@ -9,5 +9,5 @@ RESULT_DIR=/root/migrationHDP/configs.sh/$timeNow
 mkdir -p $RESULT_DIR
 for CONFIG_TYPE in `curl -s -u $AMBARI_USER:$AMBARI_PASSWORD http://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/?fields=Clusters/desired_configs | grep '" : {' | grep -v Clusters | grep -v desired_configs | cut -d'"' -f2`; do
   echo "backuping $CONFIG_TYPE"
-  /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT get $AMBARI_HOST $CLUSTER_NAME $CONFIG_TYPE | grep '^"' | grep -v '^"properties" : {' | sed "1i ##### $config_type #####" >> $RESULT_DIR/all.conf
+  /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT get $AMBARI_HOST $CLUSTER_NAME $CONFIG_TYPE | grep '^"' | grep -v '^"properties" : {' | sed "1i ##### $CONFIG_TYPE #####" >> $RESULT_DIR/all.conf
 done

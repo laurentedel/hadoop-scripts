adc4e61edbdbd27129f17ec1e3f6e423de8e2ad6
Laurent Edel <laurent.edel@gmail.com>
2018-04-05 17:00:30 +0200

    Update lr.rb
    display encoded region name only (useful for scripting regions merge for example)

--- lr.rb
+++ lr.rb
@@ -33,18 +33,19 @@ def list_regions(table_name, region_server_name = "")
 
   @end_time = Time.now
 
-  printf("%-60s | %-60s | %-15s | %-15s | %-15s | %-15s | %-15s", "SERVER_NAME", "REGION_NAME", "START_KEY", "END_KEY", "SIZE(MB)", "OCCUPANCY(%)", "REQ");
+  printf("%-60s | %-32s | %-15s | %-15s | %-15s | %-15s | %-15s", "SERVER_NAME", "ENCODED_REGION_NAME", "START_KEY", "END_KEY", "SIZE(MB)", "OCCUPANCY(%)", "REQ");
   printf("\n")
   for result in results
     # delete control characters and limit to 30 chars for StartKey and EndKey
     sk = result["startkey"].gsub!(/[^0-9A-Za-z]/, '')
     sk = "[EMPTY]" if sk.nil?
-    sk2 = sk.length > 29 ? sk.slice(1..24) + "[...]" : sk
+    startkey = sk.length > 29 ? sk.slice(1..24) + "[...]" : sk
     ek = result["endkey"].gsub!(/[^0-9A-Za-z]/, '')
     ek = "[EMPTY]" if ek.nil?
-    ek2 = ek.length > 29 ? ek.slice(1..24) + "[...]" : ek
-    printf("%-60s | %-60s | %-15s | %-15s | %-15s | %-15s | %-15s", result["server"], result["name"], sk2, ek2, result["size"], result["occupancy"], result["requests"]);
-      printf("\n")
+    endkey = ek.length > 29 ? ek.slice(1..24) + "[...]" : ek
+    encoded_region_name = result["name"].split(",")[2].split(".")[1]
+    printf("%-60s | %-32s | %-15s | %-15s | %-15s | %-15s | %-15s", result["server"], encoded_region_name, startkey, endkey, result["size"], result["occupancy"], result["requests"]);
+    printf("\n")
   end
   printf("%d rows", results.size)
 
